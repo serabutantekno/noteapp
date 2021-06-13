72d3842a6e4b29ba891153ce031dc3a76a88bd1b
Madyan <madyaneka@gmail.com>
2021-06-14 05:24:55 +0700

    database connection

---
+++ db/migrations/20210613124633-create-user.js
@@ -0,0 +1,50 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id_user: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      first_name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      last_name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      photo: {
+        type: Sequelize.STRING
+      },
+      username: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      role: {
+        type: Sequelize.ENUM('admin', 'user')
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const db = require('./db/config/database')
+const { Sequelize } = require('sequelize')
+const sequelize = new Sequelize(db.development)
+
+try {
+  sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
 
 app.listen(port, () => {
   console.log(`todoapp listening at http://localhost:${ port }`)
