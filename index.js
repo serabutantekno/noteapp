d556c4e212c955eb5789f137ecd094343321ed04
Madyan <madyaneka@protonmail.com>
2021-06-15 09:14:10 +0700

    app exception logging with sentry

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const Sentry = require('@sentry/node')
+require('dotenv').config()
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  tracesSampleRate: 1.0
+})
 
 app.use(express.json())
 
+app.use(Sentry.Handlers.requestHandler())
+
 app.use('/api/', require('./routes'))
+
+app.use(Sentry.Handlers.errorHandler())
+
 app.use(require('./helpers').errorCatcher)
 
 app.listen(port, () => {
