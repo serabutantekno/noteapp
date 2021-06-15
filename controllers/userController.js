bd51e63afe2f2cbefedd38a533f8e8a711e67d88
Madyan <madyaneka@protonmail.com>
2021-06-15 10:39:55 +0700

    retrieve user by id

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,26 @@
 const db = require('../db/models')
+const { baseResponse:RES } = require('../helpers')
 
 class userController {
 
   static async getUsers(req, res) {
     const users = await db.User.findAll()
     res.status(200).json({ message: 'retrieving all users succeed', data: users })
+  }
+
+
+  static async getUserById(req, res, next) {
+    try {
+      const user = await db.User.findOne({
+        where: {
+          id_user: req.params.id
+        }
+      })
+      user.password = undefined
+      res.status(200).json(RES.success(`User ${ user.email } retrieved successfully.`, user, res.statusCode))
+    } catch (error) {
+      next(error)
+    }
   }
 
 }
--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,6 @@ const { userController } = require('../controllers')
 const{ jwtAuthentication:jwt, authorization:roles } = require('../helpers')
 
 router.get('/', jwt, roles('admin'), userController.getUsers)
+router.get('/:id', jwt, roles('admin'), userController.getUserById)
 
 module.exports = router
